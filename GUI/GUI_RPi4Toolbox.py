99af1135ab08b3acf3ed1aaa58294bb435d0fcc4
Guillermo-Hidalgo-Gadea <66069491+Guillermo-Hidalgo-Gadea@users.noreply.github.com>
2021-02-19 12:08:11 +0100

    Update from Raspberry
    latest version working GUI on RPi4

--- GUI/GUI_RPi4Toolbox.py
+++ GUI/GUI_RPi4Toolbox.py
@@ -1,21 +1,20 @@
-"""
-Created on Tue Jan 19 15:48:01 2021
-
-@author: Guillermo Hidalgo Gadea
-"""
-
 # GUI for RPi4 Toolbox in Kivy
 import webbrowser
 from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+
+import sys
+sys.path.append('/home/hidalggc/Documents/RPi4Toolbox/Toolbox')
+from trial import Trial
 
 # Define different screens
 class MainScreen(Screen):
     def homepage(self):
         webbrowser.open("https://guillermohidalgogadea.com/#contact")
-
+    # main menu screen
 
 class SecondScreen(Screen):
     # clear input boxes
@@ -25,34 +24,73 @@ class SecondScreen(Screen):
     def clear(self):
         self.subject.text = ""
         self.experimenter.text = ""
-
+        
+    # ask for subject id and experimenter
+    # popup with subject info for re-id
 
 class ThirdScreen(Screen):
     pass
-
+ # check metadata for subject progress
+ # decide on upcoming trial
+ 
 class FourthScreen(Screen):
     pass
+ # give info on trial setup
 
 class FifthScreen(Screen):
-    pass
+    countdown_seconds = Trial().habituation_time
+    countdown_sarted = False
+        
+    def update_time(self,dt):
+        if self.countdown_sarted:
+            self.countdown_seconds -= dt
+        minutes, seconds = divmod(self.countdown_seconds, 60)
+        self.ids.countdown.text = f"{int(minutes):02}:{int(seconds):02}"    
+        
+        if self.countdown_seconds < 0.1:
+            Clock.unschedule(self.update_time)
+            self.ids.countdown.text = "00:00"
+            # change screen after countdown
+            self.parent.current = "trial"
+        
+    def on_start(self):
+        Clock.schedule_interval(self.update_time,0)
+        
+    def start_countdown(self):
+        self.on_start()
+        self.countdown_sarted = not self.countdown_sarted
+        
 
 class SixthScreen(Screen):
-    pass
+            
+    def start_trial(self):
+        # start repetitions
+        Trial().start()
+
+    def on_enter(self):
+        self.start_trial()
+        self.ids.finish.text = "Trial finished!"
+
+
+# show reps print 
+# save metadata for trial in log yaml
 
 class SeventhScreen(Screen):
+    def on_pre_enter(self):
+        self.ids.description.text = Trial().experiment.description
+    
     def edit(self):
-        open("experimentalprotocol.yaml")
+        webbrowser.open("/home/hidalggc/Documents/RPi4Toolbox/Toolbox/experiment.yaml")
 
 class EigthsScreen(Screen):
     def edit(self):
-        open("experimentalprotocol.yaml")
+        webbrowser.open("/home/hidalggc/Documents/RPi4Toolbox/Toolbox/metadata.yaml")
 
 class NinethScreen(Screen):
     pass
    
 class WindowManager(ScreenManager):
     pass
-
 
 # See outsourced GUI design in .kv file
 kv = Builder.load_file('DesignGUI.kv')
@@ -64,4 +102,3 @@ class RPi4ToolboxGUI(App):
 
 if __name__ == '__main__':
     RPi4ToolboxGUI().run()
-    
